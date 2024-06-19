56f5f5fa218f586195f288929c8398d4952b3ec7
Brad Cypert <brad.cypert@gmail.com>
2024-06-19 18:00:11 -0400

    codecrafters submit [skip ci]

--- cmd/myshell/commands/type.go
+++ cmd/myshell/commands/type.go
@@ -2,6 +2,9 @@ package commands
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 const builtin = " is a shell builtin"
@@ -9,11 +12,28 @@ const notFound = ": not found"
 
 func Type(args ...string) {
 	builtins := [...]string{"echo", "exit", "type"}
+
 	target := args[0]
 	for _, b := range builtins {
 		if b == target {
 			fmt.Printf("%s%s\n", target, builtin)
 			return
+		}
+	}
+
+	// TODO: Probably refactor this out since we're going to need this
+	// for executing programs too
+	path := os.Getenv("PATH")
+
+	if path != "" {
+		// crawl path for more things to search
+		pathFragments := strings.Split(path, ":")
+		for _, f := range pathFragments {
+			searchPath := filepath.Join(f, target)
+			if _, err := os.Stat(searchPath); err == nil {
+				fmt.Printf("%s is %s\n", target, searchPath)
+				return
+			}
 		}
 	}
 
