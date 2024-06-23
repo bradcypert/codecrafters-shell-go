585a334c75e777b8c1e9cdd1944147ae15bb126c
Brad Cypert <brad.cypert@gmail.com>
2024-06-23 11:32:34 -0400

    start on Chdir

--- cmd/myshell/commands/chdir.go
+++ cmd/myshell/commands/chdir.go
@@ -9,7 +9,7 @@ func Chdir(args ...string) error {
 	target := args[0]
 	err := os.Chdir(target)
 	if err != nil {
-		fmt.Println("cd: %s: No such file or directory", target)
+		fmt.Printf("cd: %s: No such file or directory\n", target)
 		return err
 	}
 
