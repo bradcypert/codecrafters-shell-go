64d41e092733852ad28e31d1c8cf3af96813a10d
Brad Cypert <brad.cypert@gmail.com>
2024-06-23 11:39:28 -0400

    start on home dir support

--- cmd/myshell/commands/chdir.go
+++ cmd/myshell/commands/chdir.go
@@ -3,10 +3,14 @@ package commands
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func Chdir(args ...string) error {
 	target := args[0]
+	if strings.HasPrefix(target, "~") {
+		target = strings.Replace(target, "~", os.Getenv("HOME"), 1)
+	}
 	err := os.Chdir(target)
 	if err != nil {
 		fmt.Printf("cd: %s: No such file or directory\n", target)
